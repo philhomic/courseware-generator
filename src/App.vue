f4383858a30779f943b30d654605f54677ea50d9
philhomic <philhomic@qq.com>
2017-07-11 17:10:44 +0800

    添加了侧边栏

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div id="app">
-    <div class="page">
-      <checkboxEditor></checkboxEditor>
-      <idx idx="1"></idx>
-      <component :is="radioData.type" :data="radioData" v-bind:colNum="2" v-bind:submitted="false"></component>
-      <idx idx="2"></idx>
-      <component :is="radioData2.type" :data="radioData2" v-bind:submitted="false"></component>
-      <idx idx="3"></idx>
-      <component :is="radioData3.type" :data="radioData3" v-bind:colNum="3" v-bind:submitted="false"></component>
+    <div class="left">
+      <div id="menu" class="question_type">
+        <ul>
+          <li class="type_item radio">
+            <a href="javascript:;">单选题</a>
+          </li>
+          <li class="type_item checkbox">
+            <a href="javascript:;">多选题</a>
+          </li>
+        </ul>
+      </div>
     </div>
+    <div class="right">
+      <div class="page">
+        <checkboxEditor></checkboxEditor>
+        <idx idx="1"></idx>
+        <component :is="radioData.type" :data="radioData" v-bind:colNum="2" v-bind:submitted="false"></component>
+        <idx idx="2"></idx>
+        <component :is="radioData2.type" :data="radioData2" v-bind:submitted="false"></component>
+        <idx idx="3"></idx>
+        <component :is="radioData3.type" :data="radioData3" v-bind:colNum="3" v-bind:submitted="false"></component>
+      </div>
+    </div>
+
     <!--<router-view></router-view>-->
   </div>
 </template>
@@ -171,11 +186,44 @@
   $app-bgcolor = #fff
 
   #app
-    .page
-      width: 100%
-      max-width: 1000px
-      min-width: 320px
-      margin-left: auto
-      margin-right: auto
-      background-color: $app-bgcolor
+    .left
+      position: absolute
+      top: 0
+      left: 0
+      bottom: 0
+      box-sizing: border-box
+      width: 250px
+      padding: 20px
+      background-color: #fafafa
+      #menu
+        position: absolute
+        left: 20px
+        top: 20px
+        right: 20px
+        bottom: 20px
+        overflow-y: auto
+        .type_item
+          cursor: pointer
+          height: 38px
+          line-height: 38px
+          border: 1px solid #e0e0e0
+          background-color: #fff
+          margin-bottom: 3px
+          a
+            display: block
+            height: 100%
+            font-size: 14px
+            color: #333
+            padding-left: 20px
+    .right
+      margin-left: 250px
+      .page
+        width: 100%
+        max-width: 1000px
+        min-width: 320px
+        margin-left: auto
+        margin-right: auto
+        border-left: 1px solid #e0e0e0
+        border-right: 1px solid #e0e0e0
+        background-color: $app-bgcolor
 </style>
