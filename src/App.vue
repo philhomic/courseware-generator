a27f254037561a0d82462bea4fdbcc0c48b9dde3
philhomic <philhomic@qq.com>
2017-07-27 15:04:20 +0800

    unit_editor added, redundant APP.vue code deleted

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,15 @@
 <template>
   <div id="app">
-    <div class="left">
-      <div id="menu" class="question_type">
-        <ul>
-          <li class="type_item radio">
-            <a href="javascript:;" @click="addBlock('radio')">单选题</a>
-          </li>
-          <li class="type_item checkbox">
-            <a href="javascript:;" @click="addBlock('checkbox')">多选题</a>
-          </li>
-          <li class="type_item select">
-            <a href="javascript:;" @click="addBlock('selectsingle')">下拉题</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="right">
-      <div v-for="(page, pageIndex) in pages">
-        <pageEditor :data="page" :pageIndex="pageIndex"></pageEditor>
-      </div>
-    </div>
     <router-view></router-view>
-    <editorToolbar></editorToolbar>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import pageEditor from '@/components/page_editor/page_editor';
-  import editorToolbar from '@/components/editor_toolbar/editor_toolbar';
 
  export default {
-    name: 'app',
-    components: {
-      pageEditor,
-      editorToolbar
-    },
-   computed: {
-      pages () {
-        return this.$store.state.course.pages;
-      }
-   },
-    methods: {
-      addBlock (type) {
-        this.$store.commit('addBlock', {
-          type: type
-        });
-      }
-    }
+    name: 'app'
   };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  $app-bgcolor = #fff
-
-  #app
-    .left
-      position: absolute
-      top: 0
-      left: 0
-      bottom: 0
-      box-sizing: border-box
-      width: 250px
-      padding: 20px
-      background-color: rgb(251,251,251)
-      #menu
-        position: fixed
-        width: 210px
-        overflow-y: auto
-        .type_item
-          cursor: pointer
-          height: 38px
-          line-height: 38px
-          border: 1px solid #e0e0e0
-          background-color: #fff
-          margin-bottom: 3px
-          a
-            display: block
-            height: 100%
-            font-size: 14px
-            color: #333
-            padding-left: 20px
-    .right
-      margin-left: 250px
-      border-left: 1px solid #e0e0e0
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import Router from 'vue-router';
- import page from '@/components/page/page';
+import page from '@/components/page/page';
+import unitEditor from '@/components/unit_editor/unit_editor';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    {
+      path: '/editor',
+      name: 'editor',
+      component: unitEditor
+    },
     {
       path: '/page/:pageIndex',
       name: 'page',
