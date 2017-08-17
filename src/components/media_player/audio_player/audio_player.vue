1e72aa2be4cdebf9439d846deb4a2e129b281ffc
fantaghiro <happy.wangpei@gmail.com>
2017-08-17 21:32:28 +0800

    audio_player just a beginning

--- src/components/media_player/audio_player/audio_player.vue
+++ src/components/media_player/audio_player/audio_player.vue
@@ -2,8 +2,18 @@
   .block-wrapper
     .block
       .inner
-        audio(controls="controls") Your browser does not support the #[code audio] element.
-          source(src="http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg" type="audio/wav")
+        .audio
+          audio Your browser does not support the #[code audio] element.
+            source(src="http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg" type="audio/wav")
+          button.playButton
+          .progress-control
+            .progress-holder
+              .load-progress
+              .play-progress
+                .playHead
+          .time 1:00
+          .speed 1X
+
 </template>
 
 <script type="text/ecmascript-6">
@@ -13,9 +23,10 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import '../../../assets/stylus/base.styl'
   .block-wrapper
     .block
       .inner
-        audio
-          width: 100%
+        .audio
+          background-color: $app-basecolor
 </style>
